378eef4dd375ab37e73027dfe1838428d531fc05
Fabian Triana <fabiantriana1072@gmail.com>
2022-10-06 17:02:01 -0500

    [FEATURE] Se realiza la creación del modelo con las entidades Customer, Product, Branch, Stock y UploadFile, así mismo se configura la base de datos local en PostgreSQL

--- inventario/models.py
+++ inventario/models.py
@@ -1,3 +1,72 @@
 from django.db import models
 
-# Create your models here.
+class Customer (models.Model):
+    code = models.BigIntegerField(blank=False, null=False, verbose_name='Código')
+    name = models.CharField(max_length=255, blank=False, null=False, verbose_name='Nombre')
+    phone = models.CharField(max_length=15, blank=True, null=True, verbose_name='Teléfono')
+
+    class Meta:
+        verbose_name = 'Cliente'
+        verbose_name_plural = 'Clientes'
+    
+    def __str__(self):
+        return self.name
+
+class Product (models.Model):
+    code = models.BigIntegerField(blank=False, null=False, verbose_name='Código')
+    name = models.CharField(max_length=255, blank=False, null=False, verbose_name='Nombre')
+    description = models.TextField(blank=False, verbose_name='Descripción')
+    in_stock = models.BooleanField(default=True, verbose_name='En stock')
+    is_active = models.BooleanField(default=True, verbose_name='Activo')
+    sell_price = models.DecimalField(max_digits=12, decimal_places=2, null=True, verbose_name='Valor de venta')
+    buy_price = models.DecimalField(max_digits=12, decimal_places=2, null=True, verbose_name='Valor de compra')
+
+    class Meta:
+        verbose_name = 'Producto'
+        verbose_name_plural = 'Productos'
+    
+    def __str__(self):
+        return self.name
+
+class Branch(models.Model):
+    code = models.BigIntegerField(blank=False, null=False, verbose_name='Código')
+    name = models.CharField(max_length=255, blank=False, null=False, verbose_name='Nombre')
+    phone = models.CharField(max_length=15, blank=True, null=True, verbose_name='Teléfono')    
+    address = models.CharField(max_length=15, blank=True, null=True, verbose_name='Dirección')    
+
+    class Meta:
+        verbose_name = 'Sucursal'
+        verbose_name_plural = 'Sucursales'
+    
+    def __str__(self):
+        return self.name
+
+class Stock(models.Model):
+    product = models.ForeignKey(Product, related_name='products', on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, related_name='customers', on_delete=models.CASCADE)
+    branch = models.ForeignKey(Branch, related_name='branchs', on_delete=models.CASCADE)
+    number_final = models.IntegerField(default=1, blank=False, null=False, verbose_name='Cantidad')
+    date = models.DateField(blank=False, null=False, verbose_name='Fecha')
+
+    class Meta:
+        verbose_name = 'Inventario'
+        verbose_name_plural = 'Inventarios'
+
+    def __str__(self):
+        return self.name
+
+class UploadFile(models.Model):
+    customer = models.ForeignKey(Customer, related_name='file_customers', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, blank=False, null=False, verbose_name='Nombre')
+    count_registers = models.IntegerField(default=1, blank=False, null=False, verbose_name='Cantidad de registros')
+    processsed = models.BooleanField(default=False, verbose_name='Carga procesada')
+    date = models.DateField(blank=False, null=False, verbose_name='Fecha')
+    file_path = models.FileField(upload_to='uploads/', blank=True, null=True, verbose_name='Ruta de archivo')
+
+    class Meta:
+        verbose_name = 'Archivo cargado'
+        verbose_name_plural = 'Archivos cargados'
+
+    def __str__(self):
+        return self.name
+
--- stock/settings.py
+++ stock/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'stock.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dbstock', 
+        'USER': 'postgres', 
+        'PASSWORD': os.environ['POSTGRES_DATABASE'],
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
     }
 }
 
