56382484ea5627b57838cde5db7be82c6ef5a1a4
Fabian Triana <fabiantriana1072@gmail.com>
2022-10-09 16:15:39 -0500

    [CONFIG] Se configuran las variables de entorno para la conexión al BlobStorage de Azure y se actualiza el archivo requirements.txt

--- stock/custom_azure.py
+++ stock/custom_azure.py
@@ -1,13 +1,14 @@
+import os
 from storages.backends.azure_storage import AzureStorage
 
 class AzureMediaStorage(AzureStorage):
     account_name = 'demostockdjango' # <storage_account_name>
-    account_key = 'l+6aRIkr2Ii+ZIcbavNJk3Eb07QoVfatytF6Y13b2KxoWilfs+Se1hxw8B/lyEgCSatB7HeAZhGF+AStElELOQ==' # <storage_account_key>
+    account_key = os.getenv('AZURE_STORAGE_KEY') # <storage_account_key>
     azure_container = 'media'
     expiration_secs = None
 
 class AzureStaticStorage(AzureStorage):
     account_name = 'demostockdjango' # <storage_account_name>
-    account_key = 'l+6aRIkr2Ii+ZIcbavNJk3Eb07QoVfatytF6Y13b2KxoWilfs+Se1hxw8B/lyEgCSatB7HeAZhGF+AStElELOQ==' # <storage_account_key>
+    account_key = os.getenv('AZURE_STORAGE_KEY')  # <storage_account_key>
     azure_container = 'static'
     expiration_secs = None
